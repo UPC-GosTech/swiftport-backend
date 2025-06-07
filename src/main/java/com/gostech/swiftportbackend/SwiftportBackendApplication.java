5eaa7f6282b44c374cf0fd69b7b6f2b63cb41693
ghostrider101218 <142645808+ghostrider101218@users.noreply.github.com>
2025-06-07 18:19:29 -0500

    chore: initial commit.

---
+++ src/main/java/com/gostech/swiftportbackend/SwiftportBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gostech.swiftportbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftportBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwiftportBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gostech/swiftportbackend/SwiftportBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gostech.swiftportbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftportBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
