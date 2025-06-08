3113bf4a357aa07064702c9cf59225f4ab0af8bd
natalia <natalia.roman.cruz@gmail.com>
2025-06-08 17:36:11 -0500

    Fetch: Add Zone aggregate

--- src/main/java/com/gostech/swiftportbackend/resources/domain/model/aggregates/Zone.java
+++ src/main/java/com/gostech/swiftportbackend/resources/domain/model/aggregates/Zone.java
@@ -1,18 +1,48 @@
 package com.gostech.swiftportbackend.resources.domain.model.aggregates;
 
+import com.gostech.swiftportbackend.resources.domain.model.commands.CreateZoneCommand;
 import com.gostech.swiftportbackend.resources.domain.model.valueobjects.LocationsList;
+import com.gostech.swiftportbackend.resources.domain.model.valueobjects.TenantId;
 import com.gostech.swiftportbackend.resources.domain.model.valueobjects.ZoneId;
 import com.gostech.swiftportbackend.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 
+@Getter
 @Entity
 public class Zone extends AuditableAbstractAggregateRoot<Zone> {
-    @Getter
+
     @Embedded
     private ZoneId zoneId;
 
     @Embedded
+    private TenantId tenantId;
+
+    private String name;
+
+    @Embedded
     private LocationsList locationsList;
+
+    public Zone(Long zoneId, Long tenantId, String name) {
+        this.zoneId = new ZoneId(zoneId);
+        this.tenantId = new TenantId(tenantId);
+        this.name = name;
+        this.locationsList = new LocationsList();
+    }
+
+    public Zone() {}
+
+    public Zone(CreateZoneCommand command) {
+        this.zoneId = new ZoneId(command.zoneId());
+        this.tenantId = new TenantId(command.tenantId());
+        this.name = command.name();
+        this.locationsList = new LocationsList();
+
+        /**
+         * MISSING ADD LOCATION
+         *      ON CONSTRUCTOR
+         *      AS FUNCTION
+         */
+    }
 }
