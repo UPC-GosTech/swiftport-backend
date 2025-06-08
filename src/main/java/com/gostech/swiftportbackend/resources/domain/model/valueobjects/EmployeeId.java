67402f44b45deb5a8d0eda7bdb45da582f139cdf
natalia <natalia.roman.cruz@gmail.com>
2025-06-07 20:38:38 -0500

    Fetch: Add EmployeeId

---
+++ src/main/java/com/gostech/swiftportbackend/resources/domain/model/valueobjects/EmployeeId.java
@@ -0,0 +1,12 @@
+package com.gostech.swiftportbackend.resources.domain.model.valueobjects;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public record EmployeeId(Long employeeId) {
+    public EmployeeId {
+        if (employeeId == null || employeeId <= 1) {
+            throw new NullPointerException("EmployeeId cannot be null or less than 1");
+        }
+    }
+}
--- src/main/java/com/gostech/swiftportbackend/resources/domain/model/valueobjects/VehicleId.java
+++ src/main/java/com/gostech/swiftportbackend/resources/domain/model/valueobjects/VehicleId.java
@@ -3,9 +3,9 @@ package com.gostech.swiftportbackend.resources.domain.model.valueobjects;
 import jakarta.persistence.Embeddable;
 
 @Embeddable
-public record VehicleId(Long VehicleId) {
+public record VehicleId(Long vehicleId) {
     public VehicleId {
-        if (VehicleId == null || VehicleId <= 1) {
+        if (vehicleId == null || vehicleId <= 1) {
             throw new NullPointerException("VehicleId cannot be null or less than 1");
         }
     }
