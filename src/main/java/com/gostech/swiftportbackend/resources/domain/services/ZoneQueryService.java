237d01a75e8439f7743329c67cb07d78cee26dde
natalia <natalia.roman.cruz@gmail.com>
2025-06-08 19:20:53 -0500

    Fetch: Add Zone Services

---
+++ src/main/java/com/gostech/swiftportbackend/resources/domain/services/ZoneQueryService.java
@@ -0,0 +1,13 @@
+package com.gostech.swiftportbackend.resources.domain.services;
+
+import com.gostech.swiftportbackend.resources.domain.model.aggregates.Zone;
+import com.gostech.swiftportbackend.resources.domain.model.queries.GetAllZonesQuery;
+import com.gostech.swiftportbackend.resources.domain.model.queries.GetZoneByIdQuery;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ZoneQueryService {
+    Optional<Zone> handle(GetZoneByIdQuery query);
+    List<Zone> handle(GetAllZonesQuery query);
+}
