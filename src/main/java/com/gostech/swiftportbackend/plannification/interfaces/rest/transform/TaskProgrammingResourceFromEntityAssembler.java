9cbf561f586473d97cc6a211b17e33867294ec15
natalia <natalia.roman.cruz@gmail.com>
2025-06-16 19:21:03 -0500

    Feat: Add ResourceFromEntityAssembler transforms

---
+++ src/main/java/com/gostech/swiftportbackend/plannification/interfaces/rest/transform/TaskProgrammingResourceFromEntityAssembler.java
@@ -0,0 +1,17 @@
+package com.gostech.swiftportbackend.plannification.interfaces.rest.transform;
+
+import com.gostech.swiftportbackend.plannification.domain.model.entities.TaskProgramming;
+import com.gostech.swiftportbackend.plannification.interfaces.rest.resources.TaskProgrammingResource;
+
+public class TaskProgrammingResourceFromEntityAssembler {
+    public static TaskProgrammingResource toResourceFromEntity(TaskProgramming entity) {
+        return new TaskProgrammingResource(
+                entity.getId(),
+                entity.getResourceReference().resourceType(),
+                entity.getResourceReference().resourceId(),
+                entity.getTimeInterval().start(),
+                entity.getTimeInterval().end(),
+                entity.getProgrammingStatus().toString()
+        );
+    }
+}
---
+++ src/main/java/com/gostech/swiftportbackend/plannification/interfaces/rest/transform/TaskResourceFromEntityAssembler.java
@@ -0,0 +1,15 @@
+package com.gostech.swiftportbackend.plannification.interfaces.rest.transform;
+
+import com.gostech.swiftportbackend.plannification.domain.model.entities.Task;
+import com.gostech.swiftportbackend.plannification.interfaces.rest.resources.TaskResource;
+
+public class TaskResourceFromEntityAssembler {
+    public static TaskResource toResourceFromEntity(Task entity) {
+        return new TaskResource(
+                entity.getId(),
+                entity.getDescription(),
+                entity.getStatus().toString(),
+                entity.getTitle()
+        );
+    }
+}
