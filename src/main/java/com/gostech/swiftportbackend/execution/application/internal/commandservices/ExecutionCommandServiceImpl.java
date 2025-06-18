a98d37bd1deb9f03ce7530a315e1c79f78b9442c
natalia <natalia.roman.cruz@gmail.com>
2025-06-18 18:49:53 -0500

    Feat: Add IncidentReportCommandServiceImpl

--- src/main/java/com/gostech/swiftportbackend/execution/application/internal/commandservices/ExecutionCommandServiceImpl.java
+++ src/main/java/com/gostech/swiftportbackend/execution/application/internal/commandservices/ExecutionCommandServiceImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 public class ExecutionCommandServiceImpl implements ExecutionCommandService {
     private final ExecutionRepository executionRepository;
 
-    public  ExecutionCommandServiceImpl(ExecutionRepository executionRepository) {
+    public ExecutionCommandServiceImpl(ExecutionRepository executionRepository) {
         this.executionRepository = executionRepository;
     }
 
