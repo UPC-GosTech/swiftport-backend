044e867bc5805abbfb0918449816194f9b3d4095
natalia <natalia.roman.cruz@gmail.com>
2025-07-02 15:23:50 -0500

    Feat: Add execution acl

---
+++ src/main/java/com/gostech/swiftportbackend/execution/application/acl/ExecutionContextFacadeImpl.java
@@ -0,0 +1,36 @@
+package com.gostech.swiftportbackend.execution.application.acl;
+
+import com.gostech.swiftportbackend.execution.domain.model.commands.CreateExecutionCommand;
+import com.gostech.swiftportbackend.execution.domain.services.ExecutionCommandService;
+import com.gostech.swiftportbackend.execution.domain.services.ExecutionQueryService;
+import com.gostech.swiftportbackend.execution.interfaces.acl.ExecutionContextFacade;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class ExecutionContextFacadeImpl implements ExecutionContextFacade {
+    private final ExecutionCommandService executionCommandService;
+    private final ExecutionQueryService executionQueryService;
+
+    public ExecutionContextFacadeImpl(ExecutionCommandService executionCommandService, ExecutionQueryService executionQueryService) {
+        this.executionCommandService = executionCommandService;
+        this.executionQueryService = executionQueryService;
+    }
+
+    public Long createExecution(
+            Long taskProgrammingId,
+            String taskExecutionStatus,
+            LocalDateTime start,
+            LocalDateTime end,
+            Long tenantId) {
+        var createExecutionCommand = new CreateExecutionCommand(
+                taskProgrammingId,
+                taskExecutionStatus,
+                start,
+                end,
+                tenantId
+        );
+        return executionCommandService.handle(createExecutionCommand);
+    }
+}
