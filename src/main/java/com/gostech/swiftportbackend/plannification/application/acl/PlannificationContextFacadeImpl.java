acdb919e98f702625a38d7fc58505fb0defb31b8
natalia <natalia.roman.cruz@gmail.com>
2025-07-02 16:43:00 -0500

    Feat: Add plannification acl

---
+++ src/main/java/com/gostech/swiftportbackend/plannification/application/acl/PlannificationContextFacadeImpl.java
@@ -0,0 +1,47 @@
+package com.gostech.swiftportbackend.plannification.application.acl;
+
+import com.gostech.swiftportbackend.execution.domain.services.ExecutionCommandService;
+import com.gostech.swiftportbackend.plannification.domain.model.commands.CreateActivityCommand;
+import com.gostech.swiftportbackend.plannification.domain.services.ActivityCommandService;
+import com.gostech.swiftportbackend.plannification.domain.services.ActivityQueryService;
+import com.gostech.swiftportbackend.plannification.interfaces.acl.PlannificationContextFacade;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class PlannificationContextFacadeImpl implements PlannificationContextFacade {
+    private final ActivityCommandService activityCommandService;
+    private final ActivityQueryService activityQueryService;
+
+    public PlannificationContextFacadeImpl(ActivityCommandService activityCommandService, ActivityQueryService activityQueryService) {
+        this.activityCommandService = activityCommandService;
+        this.activityQueryService = activityQueryService;
+    }
+
+    public Long createActivity(
+            String activityCode,
+            String description,
+            LocalDateTime expectedTime,
+            Integer weekNumber,
+            String activityStatus,
+            Long zoneOrigin,
+            Long locationOrigin,
+            Long zoneDestination,
+            Long locationDestination,
+            Long tenantId){
+        var createActivityCommand = new CreateActivityCommand(
+                activityCode,
+                description,
+                expectedTime,
+                weekNumber,
+                activityStatus,
+                zoneOrigin,
+                locationOrigin,
+                zoneDestination,
+                locationDestination,
+                tenantId
+        );
+        return activityCommandService.handle(createActivityCommand);
+    }
+}
